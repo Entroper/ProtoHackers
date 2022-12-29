b58e688b9aee378dcc6e024dff84c88071c89dee
Entroper <entroper@gmail.com>
2022-12-28 20:05:41 -0500

    finish problem 5

--- ProtoHackers/TotallyLegitChatClientService.cs
+++ ProtoHackers/TotallyLegitChatClientService.cs
@@ -1,11 +1,17 @@
 ﻿using System.IO.Pipelines;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace ProtoHackers;
 
 public class TotallyLegitChatClientService : ITcpService
 {
+	public readonly Regex BoguscoinAddressRegex = new Regex("(?<=(^| ))7[0-9A-Za-z]{25,34}(?=($| ))", RegexOptions.Compiled);
+	public readonly string TonyBoguscoinAddressReplacement = "7YWHMfk9JZe0LM0g1ZauHuiSxhI";
+	private static readonly byte[] NewLine = Encoding.ASCII.GetBytes("\n");
+
 	private SocketPipe _incoming;
 	private SocketPipe _outgoing;
 
@@ -29,15 +35,30 @@ public class TotallyLegitChatClientService : ITcpService
 		outgoingSocket.SetSocketOption(SocketOptionLevel.IPv6, SocketOptionName.IPv6Only, false);
 		await outgoingSocket.ConnectAsync(_upstream);
 		_outgoing = new SocketPipe(outgoingSocket);
+		Console.WriteLine($"Connected to upstream server at {_upstream}");
 
 		await Task.WhenAll(
 			_incoming.SocketToPipe(),
 			_incoming.PipeToSocket(),
 			_outgoing.SocketToPipe(),
 			_outgoing.PipeToSocket(),
-			PassTotallyLegitMessages(_incoming.Input, _outgoing.Output),
-			PassTotallyLegitMessages(_outgoing.Input, _incoming.Output)
+			ClientToServer(),
+			ServerToClient()
 		);
+
+		_server.Disconnect(_id);
+	}
+
+	private async Task ClientToServer()
+	{
+		await PassTotallyLegitMessages(_incoming.Input, _outgoing.Output);
+		await _outgoing.Output.CompleteAsync();
+	}
+
+	private async Task ServerToClient()
+	{
+		await PassTotallyLegitMessages(_outgoing.Input, _incoming.Output);
+		await _incoming.Output.CompleteAsync();
 	}
 
 	private async Task PassTotallyLegitMessages(PipeReader reader, PipeWriter writer)
@@ -45,7 +66,13 @@ public class TotallyLegitChatClientService : ITcpService
 		var lineReader = new LineReader(reader);
 		await foreach (var line in lineReader.ReadLines(CancellationToken.None))
 		{
-			
+			var message = EncodingExtensions.GetString(Encoding.ASCII, line);
+			Console.WriteLine(message);
+			var matches = BoguscoinAddressRegex.Match(message);
+			message = BoguscoinAddressRegex.Replace(message, TonyBoguscoinAddressReplacement);
+			EncodingExtensions.Convert(Encoding.ASCII.GetEncoder(), message.AsSpan(), writer, true, out _, out _);
+			await writer.WriteAsync(NewLine);
+			await writer.FlushAsync();
 		}
 	}
 }
